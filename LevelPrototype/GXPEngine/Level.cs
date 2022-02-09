9a15543d55c118b2a91bfd370aaa98d4c0c2453a
Kamila Matuszak <kamila.matuszak@list.pl>
2022-02-09 14:22:24 +0100

    Person_Pickup
    Tiled level implemented, person class acting as a pickup done

---
+++ LevelPrototype/GXPEngine/Level.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using GXPEngine;
+using TiledMapParser;
+
+
+internal class Level : GameObject
+{
+    TiledLoader loader;
+    Random rand = new Random();
+    public Level(string filename)
+    {
+        loader = new TiledLoader(filename);
+        StartLevel();
+    }
+    private void StartLevel(bool includeImageLayers = true)
+    {
+        loader.addColliders = false;
+        loader.rootObject = game;
+        loader.LoadImageLayers();
+        loader.rootObject = this;
+        loader.LoadTileLayers(0);
+        loader.addColliders = true;
+        loader.LoadTileLayers(1);
+        loader.LoadTileLayers(2);
+        loader.LoadTileLayers(3);
+        loader.autoInstance = true;
+        loader.LoadObjectGroups();
+
+
+    }
+}
+
+
---
+++ LevelPrototype/GXPEngine/MyGame.cs
@@ -0,0 +1,40 @@
+using System;									// System contains a lot of default C# libraries 
+using GXPEngine;                                // GXPEngine contains the engine
+using System.Drawing;                           // System.Drawing contains drawing tools such as Color definitions
+using System.Collections.Generic;
+
+public class MyGame : Game
+{
+	string levelName = "map_prototype2.tmx";
+	public MyGame() : base(1366, 768, false)		// Create a window that's 800x600 and NOT fullscreen
+	{
+		LoadLevel(levelName);
+	}
+
+	// For every game object, Update is called every frame, by the engine:
+	void Update()
+	{
+		// Empty
+	}
+
+	static void Main()							// Main() is the first method that's called when the program is run
+	{
+		new MyGame().Start();					// Create a "MyGame" and start it
+	}
+
+	private void DestroyAll()
+	{
+		List<GameObject> children = GetChildren();
+		foreach (GameObject child in children)
+		{
+			child.LateDestroy();
+		}
+	}
+
+
+	void LoadLevel(string filename)
+	{
+		DestroyAll();
+		LateAddChild(new Level(filename));
+	}
+}
